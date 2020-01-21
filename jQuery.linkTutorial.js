7714e90795efecd44ebf86797c56547a76889584
Thato Morare <thato@websprojects.co.za>
2020-01-21 17:13:51 +0200

    0.2.0 release

---
+++ jQuery.linkTutorial.js
@@ -0,0 +1,37 @@
+/*
+ * name: LinkIt
+ * author: True Saint
+ * version 0.2.0
+ * license: MIT
+ */
+
+(function ($) {
+    $.fn.linkIt = function (options) {
+        //Default Settings
+        var settings = $.extend({
+            href: null,
+            text: null,
+            target: '_self'
+        }, options);
+
+        //Validate
+        if(settings.href == null || settings.href == ''){
+            console.log('you need to add an href option to use LinkIt');
+            return this;
+        }
+
+        return this.each(function () {
+            var object = $(this);
+
+            if(settings.text == null){
+                settings.text = object.text();
+            }
+
+
+            object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+        });
+
+
+
+    }
+}(jQuery));
